4c709c059bae540204999f76de665ac132daa2e4
nmeilick <git@ac.ganyme.de>
2019-02-06 22:43:51 +0100

    Changed order of search paths

--- os_linux.go
+++ os_linux.go
@@ -8,17 +8,14 @@ import (
 // SearchPaths returns application specific search paths, from most specific
 // to least specific.
 func (p *Properties) SearchPaths() (paths []string) {
-	if p.Name != "" {
-		if path := os.Getenv("HOME"); path != "" {
-			paths = append(paths, filepath.Join(path, "."+p.Name))
-		}
-	}
-
 	if p.Dir != "" {
 		paths = append(paths, p.Dir)
 	}
 
 	if p.Name != "" {
+		if path := os.Getenv("HOME"); path != "" {
+			paths = append(paths, filepath.Join(path, "."+p.Name))
+		}
 		paths = append(paths, filepath.Join("/etc", p.Name))
 	}
 	return
--- os_windows.go
+++ os_windows.go
@@ -8,14 +8,15 @@ import (
 // SearchPaths returns application specific search paths, from most specific
 // to least specific.
 func (p *Properties) SearchPaths() (paths []string) {
+	if p.Dir != "" {
+		paths = append(paths, p.Dir)
+	}
+
 	if p.Name != "" {
 		if path := os.Getenv("USERPROFILE"); path != "" {
 			paths = append(paths, filepath.Join(path, "."+p.Name))
 		}
 	}
 
-	if p.Dir != "" {
-		paths = append(paths, p.Dir)
-	}
 	return
 }
